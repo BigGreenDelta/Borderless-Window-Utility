e4e43e2c4e0ead8b641f62b7bd012ae203060187
.AJIADb9 <ajiadb9@bgd.team>
2025-04-14 03:26:38 +0100

    Added save/load preset buttons and rearranged UI

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import sys
+from pathlib import Path
+
 import win32gui
 import ctypes
 import win32con
@@ -20,10 +23,12 @@ HWND_KEY = "hwnd"
 
 # Event Keys
 EXIT_EVENT = "exit"
-RESIZE_EVENT = "resize"
+RESIZE_EVENT = "Apply"
 BORDERLESS_WINDOW_EVENT = "Borderless Window"
-REVERT_CHANGES_EVENT = "Revert Changes"
+REVERT_CHANGES_EVENT = "Normal Window"
 REFRESH_EVENT = "Refresh"
+SAVE_PROFILE_EVENT = "Save"
+LOAD_PROFILE_EVENT = "Load"
 COMBO_KEY_EVENT = "combo"
 
 
@@ -39,14 +44,21 @@ V_RES = 0
 WINDOW: sg.Window | None = None
 
 
+def profile_file_path() -> Path:
+    script_path = Path(sys.argv[0])
+    if script_path is not None:
+        return Path(script_path.parent).joinpath(PROFILE_FILE)
+    else:
+        return Path(PROFILE_FILE)
+
+
 def read_profiles():
     global PROFILES
 
     config = configparser.ConfigParser()
-    config.read(PROFILE_FILE)
+    config.read(profile_file_path())
 
     for section in config.sections():
-        print(f"Found profile: {section}")
         PROFILES[section] = {
             X_KEY: config[section][X_KEY],
             Y_KEY: config[section][Y_KEY],
@@ -55,6 +67,34 @@ def read_profiles():
         }
 
     return PROFILES
+
+
+def save_window_profile(hwnd):
+    global X_KEY, Y_KEY, H_RES_KEY, V_RES_KEY, X_POS, Y_POS, H_RES, V_RES, WINDOW
+
+    window_size_update(hwnd, from_ui=True)
+
+    title = win32gui.GetWindowText(hwnd)
+    if not title:
+        sg.popup_error("Please select a window first.")
+        return
+
+    profile_path = profile_file_path()
+
+    config = configparser.ConfigParser()
+    config.read(profile_path)
+
+    output_config = Path(profile_path)
+    profile_path.parent.mkdir(exist_ok=True, parents=True)
+    with output_config.open("w") as output_file:
+        config.remove_section(title)
+        config.add_section(title)
+        config[title][X_KEY] = str(X_POS)
+        config[title][Y_KEY] = str(Y_POS)
+        config[title][WIDTH_KEY] = str(H_RES)
+        config[title][HEIGHT_KEY] = str(V_RES)
+
+        config.write(output_file)
 
 
 def load_profile(profiles, title):
@@ -71,34 +111,45 @@ def refresh_all_visible_windows():
     VISIBLE_WINDOWS_TITLES_LIST = list(ALL_VISIBLE_WINDOWS.keys())
     VISIBLE_WINDOWS_TITLES_LIST.sort(key=lambda x: x in PROFILES, reverse=True)
     if WINDOW is not None:
-        WINDOW.Element(COMBO_KEY_EVENT).update(values=VISIBLE_WINDOWS_TITLES_LIST)
+        WINDOW[COMBO_KEY_EVENT].update(values=VISIBLE_WINDOWS_TITLES_LIST)
 
 
-def window_size_update(hwnd):
+def window_size_update(hwnd, *, from_ui=False):
     global X_POS, Y_POS, H_RES, V_RES
 
-    window_rect = win32gui.GetWindowRect(hwnd)
-    title = win32gui.GetWindowText(hwnd)
-    profiles = read_profiles()
-    profile = load_profile(profiles, title)
-    if profile is not None:
-        WINDOW.Element(X_KEY).update(profile[X_KEY])
-        X_POS = int(profile[X_KEY])
-        WINDOW.Element(Y_KEY).update(profile[Y_KEY])
-        Y_POS = int(profile[Y_KEY])
-        WINDOW.Element(H_RES_KEY).update(profile[WIDTH_KEY])
-        H_RES = int(profile[WIDTH_KEY])
-        WINDOW.Element(V_RES_KEY).update(profile[HEIGHT_KEY])
-        V_RES = int(profile[HEIGHT_KEY])
+    if from_ui:
+        x_pos = WINDOW[X_KEY].get()
+        y_pos = WINDOW[Y_KEY].get()
+        h_res = WINDOW[H_RES_KEY].get()
+        v_res = WINDOW[V_RES_KEY].get()
     else:
-        WINDOW.Element(X_KEY).update(int(window_rect[0]))
-        X_POS = int(window_rect[0])
-        WINDOW.Element(Y_KEY).update(int(window_rect[1]))
-        Y_POS = int(window_rect[1])
-        WINDOW.Element(H_RES_KEY).update(int(window_rect[2] - window_rect[0]))
-        H_RES = int(window_rect[2] - window_rect[0])
-        WINDOW.Element(V_RES_KEY).update(int(window_rect[3] - window_rect[1]))
-        V_RES = int(window_rect[3] - window_rect[1])
+        title = win32gui.GetWindowText(hwnd)
+        profiles = read_profiles()
+        profile = load_profile(profiles, title)
+        if profile is not None:
+            x_pos = profile[X_KEY]
+            y_pos = profile[Y_KEY]
+            h_res = profile[WIDTH_KEY]
+            v_res = profile[HEIGHT_KEY]
+        else:
+            window_rect = win32gui.GetWindowRect(hwnd)
+            x_pos = window_rect[0]
+            y_pos = window_rect[1]
+            h_res = window_rect[2] - window_rect[0]
+            v_res = window_rect[3] - window_rect[1]
+
+    try:
+        X_POS = int(x_pos)
+        Y_POS = int(y_pos)
+        H_RES = int(h_res)
+        V_RES = int(v_res)
+    except:
+        return
+
+    WINDOW[X_KEY].update(X_POS)
+    WINDOW[Y_KEY].update(Y_POS)
+    WINDOW[H_RES_KEY].update(H_RES)
+    WINDOW[V_RES_KEY].update(V_RES)
 
 
 def win_enum_handler(hwnd, ctx):
@@ -116,33 +167,49 @@ def create_window():
     global WINDOW
     sg.theme("DarkGrey11")
     layout = [
-        [sg.Text("Window Selection: ")],
+        [sg.HorizontalSeparator(), sg.Text("Window", font="bold"), sg.HorizontalSeparator()],
         [
             sg.Combo(
                 VISIBLE_WINDOWS_TITLES_LIST,
                 enable_events=True,
                 key=COMBO_KEY_EVENT,
-                size=(100, 1),
+                size=50,
                 auto_size_text=False,
             ),
             sg.Button(REFRESH_EVENT),
         ],
+        [sg.HorizontalSeparator(), sg.Text("Positioning", font="bold"), sg.HorizontalSeparator()],
         [
-            sg.Text("X Pos:"),
-            sg.InputText("0", size=(7, 7), key=X_KEY),
-            sg.Text("Y Pos:"),
-            sg.InputText("0", size=(7, 7), key=Y_KEY),
+            sg.Text("Preset:", size=7),
+            sg.Button(SAVE_PROFILE_EVENT, size=9),
+            sg.Button(LOAD_PROFILE_EVENT, size=9),
         ],
         [
-            sg.Text("H Res:"),
-            sg.InputText("2560", size=(7, 7), key=H_RES_KEY),
-            sg.Text("V Res:"),
-            sg.InputText("1440", size=(7, 7), key=V_RES_KEY),
+            sg.Text("Position:", size=7),
+            sg.Text("X", size=1),
+            sg.InputText("0", size=7, key=X_KEY),
+            sg.Text("Y", size=1),
+            sg.InputText("0", size=7, key=Y_KEY),
         ],
         [
-            sg.Button(BORDERLESS_WINDOW_EVENT),
-            sg.Button(REVERT_CHANGES_EVENT),
-            sg.Button("Resize/Position", key=RESIZE_EVENT),
+            sg.Text("Size:", size=7),
+            sg.Text("H", 1),
+            sg.InputText("2560", size=7, key=H_RES_KEY),
+            sg.Text("V", 1),
+            sg.InputText("1440", size=7, key=V_RES_KEY),
+        ],
+        [
+            sg.Text("", size=7),
+            sg.Button(RESIZE_EVENT, size=9),
+        ],
+        [sg.HorizontalSeparator(), sg.Text("Style", font="bold"), sg.HorizontalSeparator()],
+        [
+            sg.Button(BORDERLESS_WINDOW_EVENT, size=16),
+            sg.Button(REVERT_CHANGES_EVENT, size=16),
+        ],
+        [sg.HorizontalSeparator()],
+        [
+            sg.Push(),
             sg.Exit(key=EXIT_EVENT),
         ],
     ]
@@ -199,7 +266,7 @@ def try_auto_borderless():
 
     if found_profile is not None and hwnd is not None:
         print(f"Found the only one profile: '{found_profile}', applying settings")
-        WINDOW.Element(COMBO_KEY_EVENT).update(value=found_profile)
+        WINDOW[COMBO_KEY_EVENT].update(value=found_profile)
         auto_borderless(hwnd)
 
 
@@ -257,12 +324,8 @@ def main():
                 print("Window not found, reloading list")
                 refresh_all_visible_windows()
             else:
-                X_POS = int(values[X_KEY])
-                Y_POS = int(values[Y_KEY])
-                H_RES = int(values[H_RES_KEY])
-                V_RES = int(values[V_RES_KEY])
+                window_size_update(hwnd, from_ui=True)
                 user32.MoveWindow(hwnd, X_POS, Y_POS, H_RES, V_RES, True)
-                window_size_update(hwnd)
 
         if event == REFRESH_EVENT:
             refresh_all_visible_windows()
@@ -275,6 +338,12 @@ def main():
                 refresh_all_visible_windows()
             else:
                 auto_borderless(hwnd)
+
+        if event == SAVE_PROFILE_EVENT:
+            save_window_profile(hwnd)
+
+        if event == LOAD_PROFILE_EVENT:
+            window_size_update(hwnd)
 
 
 if __name__ == "__main__":
